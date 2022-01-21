028d08ce5ba91c378b045f1068648d5623f2ef9d
SheriffPepper <Chistoffd@yandex.ru>
2022-01-21 17:23:32 +0300

    Changing some sprite-load logic

--- load_data.py
+++ load_data.py
@@ -2,6 +2,17 @@ import pygame
 import os
 
 
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, image: str, *groups) -> None:
+        super().__init__(*groups)
+
+        self.image = load_image(image)
+        self.rect = self.image.get_rect()
+
+    def draw(self, screen: pygame.Surface) -> None:
+        screen.blit(self.image, self.rect)
+
+
 def load_image(image: str) -> pygame.image:
     # Image was not found or is not a file
     if not (os.path.exists(image) and os.path.isfile(image)):
@@ -17,16 +28,16 @@ with open('./data/data.path', 'r', encoding='utf-8') as file:
 
 
 # Welcome window background
-welcome_background = load_image(welcome_background)
+welcome_background = Sprite(welcome_background)
 
 # Entities
-pacman = load_image(pacman)
+pacman = Sprite(pacman)
 
-blinky = load_image(blinky)  # red ghost
-pinky = load_image(pinky)  # pink ghost
-inky = load_image(inky)  # blue ghost
-clyde = load_image(clyde)  # orange ghost
+blinky = Sprite(blinky)  # red ghost
+pinky = Sprite(pinky)  # pink ghost
+inky = Sprite(inky)  # blue ghost
+clyde = Sprite(clyde)  # orange ghost
 
 # Decoration textures
-symbols = load_image(symbols)
-items = load_image(items)
+symbols = Sprite(symbols)
+items = Sprite(items)
