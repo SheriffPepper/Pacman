945c076897a78876a72b2132b934b6d52bfb0b96
SheriffPepper <Chistoffd@yandex.ru>
2022-01-21 17:07:32 +0300

    Auto picture-loading system added

--- load_data.py
+++ load_data.py
@@ -8,3 +8,25 @@ def load_image(image: str) -> pygame.image:
         exit(f"Image file \"{image}\" not found")
 
     return pygame.image.load(image)
+
+
+# Automatic data-load
+with open('./data/data.path', 'r', encoding='utf-8') as file:
+    # Data-loader
+    exec(file.read())
+
+
+# Welcome window background
+welcome_background = load_image(welcome_background)
+
+# Entities
+pacman = load_image(pacman)
+
+blinky = load_image(blinky)  # red ghost
+pinky = load_image(pinky)  # pink ghost
+inky = load_image(inky)  # blue ghost
+clyde = load_image(clyde)  # orange ghost
+
+# Decoration textures
+symbols = load_image(symbols)
+items = load_image(items)
