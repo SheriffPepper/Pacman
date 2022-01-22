5b46cf0a285484ce333a25483c22056739db9fb9
SheriffPepper <Chistoffd@yandex.ru>
2022-01-22 09:51:17 +0300

    Added an entry window

--- windows.py
+++ windows.py
@@ -3,7 +3,46 @@ import pygame
 import sys
 
 
+def start_game(screen: pygame.Surface) -> None:
+    """Asks what's your nickname"""
+    width, height = 800, 600
+
+    screen.fill(black, (0, height // 2, width, height))
+    nick = ''
+
+    screen_print(screen, "Your nickname -", (100, height // 2 + 24))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                raise SystemExit
+            elif event.type == pygame.KEYDOWN:
+                key = event.key
+                # Esc - return (warning: no more, than 1000 times)
+                if key == pygame.K_ESCAPE:
+                    nick = ''
+                    return main_menu(screen)
+                # Print some text
+                elif pygame.K_a <= key <= pygame.K_z or pygame.K_0 <= key <= pygame.K_9 or \
+                        key == pygame.K_SPACE:
+                    nick += chr(key)
+                # Delete one symbol
+                elif key == pygame.K_BACKSPACE:
+                    nick = nick[:-1]
+                # Enter the name
+                elif key == pygame.K_RETURN:
+                    if nick:
+                        return nick
+        nick = nick[:20]
+
+        screen.fill(black, (356, height // 2 + 24, 320, 16))
+        screen_print(screen, nick, (356, height // 2 + 24))
+        pygame.display.flip()
+
+
 def main_menu(screen: pygame.Surface) -> None:
+    width, height = 800, 600
+
     pygame.display.set_caption("Welcome to Pacman Classic!")
 
     # Welcome picture
@@ -62,7 +101,7 @@ def main_menu(screen: pygame.Surface) -> None:
                 # Start Game button
                 if start_button_pos[0] <= x <= start_button_pos[0] + 160 and \
                         start_button_pos[1] <= y <= start_button_pos[1] + 16:
-                    print("Start Game")
+                    return start_game(screen)
                 # Settings button
                 elif settings_button_pos[0] <= x <= settings_button_pos[0] + 128 and \
                         settings_button_pos[1] <= y <= settings_button_pos[1] + 16:
@@ -74,13 +113,10 @@ def main_menu(screen: pygame.Surface) -> None:
                 # Exit button
                 elif exit_button_pos[0] <= x <= exit_button_pos[0] + 64 and \
                      exit_button_pos[1] <= y <= exit_button_pos[1] + 16:
-                    print("Exit")
                     raise SystemExit
                 # Secret button
                 elif 315 <= x <= 465 and 115 <= y <= 270:
                     print("Secret button!")
-                else:
-                    pass
 
         # Pseudo-buttons
         screen_print(screen, "Start Game", start_button_pos,
@@ -93,8 +129,9 @@ def main_menu(screen: pygame.Surface) -> None:
                      color=yellow if exit_button else None)
 
         pygame.display.flip()
-        clock.tick(60)
 
+
+nick = ''
 
 if __name__ == "__main__":
     pygame.init()
@@ -104,6 +141,8 @@ if __name__ == "__main__":
     clock = pygame.time.Clock()
 
     main_menu(screen)
+
+    print(nick)
 
     print("Main menu closed successfully!")
     pygame.quit()
